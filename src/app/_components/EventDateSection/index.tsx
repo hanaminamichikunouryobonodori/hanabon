804844c950c4e343c98dd900cf6a87b0a29e1fa5
花南地区盆踊り実行委員会 <hanaminamibonodori@gmail.com>
2025-07-03 21:24:52 +0900

    fix(build):不要なconsoleの削除

--- src/app/_components/EventDateSection/index.tsx
+++ src/app/_components/EventDateSection/index.tsx
@@ -5,11 +5,6 @@ import { PageData } from '@/types/microCMS';
 import style from './eventDate.module.scss';
 
 const EventDateSection = ({ data }: { data: PageData }) => {
-  data.content.map((block) => {
-    if (block.fieldId === 'two_column_block') {
-      console.log(block);
-    }
-  });
   return (
     <div className={style.container}>
       <MinchoHeadingComponent level={2}>{data.title}</MinchoHeadingComponent>
