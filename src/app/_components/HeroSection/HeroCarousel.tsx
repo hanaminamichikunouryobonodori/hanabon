52c01e5acbd85b99fd34c6f094ca5ad2dde959b4
花南地区盆踊り実行委員会 <hanaminamibonodori@gmail.com>
2025-07-07 18:30:50 +0900

    fix: Heroに読み込み時のdelayを追加

--- src/app/_components/HeroSection/HeroCarousel.tsx
+++ src/app/_components/HeroSection/HeroCarousel.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Image from 'next/image';
 import { Autoplay, EffectFade } from 'swiper/modules';
@@ -16,43 +16,68 @@ interface CarouselProps {
 }
 
 const HeroCarouselComponent: React.FC<CarouselProps> = ({ backgroundImageUrls, logoImageData }) => {
+  const [showCarousel, setShowCarousel] = useState(false);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowCarousel(true);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   if (!logoImageData) {
     return null;
   }
 
   return (
     <div className={styles.carouselContainer}>
-      <Swiper
-        autoplay={{
-          delay: 8000,
-          disableOnInteraction: false,
-        }}
-        className={styles.carousel}
-        effect='fade'
-        loop={true}
-        modules={[Autoplay, EffectFade]}
-        speed={2000}
-      >
-        {backgroundImageUrls.map((imageUrl, index) => (
-          <SwiperSlide key={index}>
+      {showCarousel ? (
+        <Swiper
+          autoplay={{
+            delay: 8000,
+            disableOnInteraction: false,
+          }}
+          className={styles.carousel}
+          effect='fade'
+          loop={true}
+          modules={[Autoplay, EffectFade]}
+          speed={2000}
+        >
+          {backgroundImageUrls.map((imageUrl, index) => (
+            <SwiperSlide key={index}>
+              <Image
+                alt={`Hero Background ${index + 1}`}
+                className={styles.carouselImage}
+                fill
+                priority={index === 0}
+                sizes='100vw'
+                src={imageUrl}
+                style={{ objectFit: 'cover' }}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      ) : (
+        <div className={styles.carouselContainer}>
+          <div className={styles.carousel}>
             <Image
-              alt={`Hero Background ${index + 1}`}
+              alt={`Hero Background`}
               className={styles.carouselImage}
               fill
-              priority={index === 0}
               sizes='100vw'
-              src={imageUrl}
-              style={{ objectFit: 'cover' }}
+              src={backgroundImageUrls[0]}
+              style={{ objectFit: 'cover', scale: '1.3' }}
             />
-          </SwiperSlide>
-        ))}
-      </Swiper>
+          </div>
+        </div>
+      )}
       <Image
         alt='花南地区納涼盆踊り'
         className={styles.heroLogoImage}
         height={logoImageData.height}
         priority
         src={logoImageData.url}
+        style={{ objectFit: 'cover' }}
         width={logoImageData.width}
       />
     </div>
