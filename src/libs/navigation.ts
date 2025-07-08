9e6d618221f146c55665b031915061154bc8c0ff
花南地区盆踊り実行委員会 <hanaminamibonodori@gmail.com>
2025-07-09 01:35:56 +0900

    feat(Footer): フッターに項目を追加

--- src/libs/navigation.ts
+++ src/libs/navigation.ts
@@ -1,8 +1,10 @@
 export const menuItems = [
   { to: 'eventDate', label: '開催日程' },
   { to: 'about', label: 'どんなお祭り？' },
+  { to: 'program', label: 'お祭りの内容' },
   { to: 'gallery', label: 'お祭りの様子' },
   { to: 'news', label: 'お知らせ' },
+  { to: 'joinCommittee', label: 'サポーター募集' },
   { to: 'access', label: 'アクセス' },
   { to: 'contact', label: '問い合わせ' },
   { to: 'sponsorship', label: '主催・協賛' },
