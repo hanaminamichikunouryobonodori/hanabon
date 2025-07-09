b53218fa96af70cafa1fcbf9fa5770b6f677001f
花南地区盆踊り実行委員会 <hanaminamibonodori@gmail.com>
2025-07-09 14:37:57 +0900

    fix: sitemapを動的に読み込む

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -5,6 +5,8 @@ import { NewsData } from '@/types';
 
 const BASE_URL = process.env.NEXT_PUBLIC_DOMAIN || 'https://hanabon.vercel.app';
 
+export const revalidate = 60;
+
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const defaultPages: MetadataRoute.Sitemap = [
     {
