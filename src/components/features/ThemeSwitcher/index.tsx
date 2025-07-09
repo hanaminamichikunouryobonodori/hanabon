1c10ed8ad7224257f4380cb55087f0f07c53aded
花南地区盆踊り実行委員会 <hanaminamibonodori@gmail.com>
2025-07-09 22:33:11 +0900

    fix(Theme): カラーモードを変えるアイコンをトグルに変更

--- src/components/features/ThemeSwitcher/index.tsx
+++ src/components/features/ThemeSwitcher/index.tsx
@@ -5,51 +5,49 @@ import { useState, useEffect, type FC } from 'react';
 import { useTheme } from 'next-themes';
 import { HiSun, HiMoon } from 'react-icons/hi2';
 
+import styles from './ThemeSwitcher.module.scss';
+
 interface ThemeSwitcherProps {
   className?: string;
-  onClick?: () => void;
 }
 
-export const ThemeSwitcher: FC<ThemeSwitcherProps> = ({ className, onClick }) => {
+export const ThemeSwitcher: FC<ThemeSwitcherProps> = ({ className }) => {
   const [mounted, setMounted] = useState(false);
   const { resolvedTheme, setTheme } = useTheme();
 
   useEffect(() => {
     setMounted(true);
   }, []);
-  const handleThemeChange = (newTheme: string) => {
-    document.body.classList.add('theme-is-changing');
-
-    setTimeout(() => {
-      setTheme(newTheme);
-      setTimeout(() => {
-        document.body.classList.remove('theme-is-changing');
-      }, 50);
-    }, 200);
-  };
-
-  const toggleTheme = () => {
-    const newTheme = resolvedTheme === 'dark' ? 'light' : 'dark';
-    handleThemeChange(newTheme);
-    if (onClick) {
-      onClick();
-    }
-  };
 
   if (!mounted) {
-    return <div className={className} />;
+    // 外部からのclassNameも適用できるようにする
+    return <div className={`${className || ''} ${styles.themeSwitchPlaceholder}`} />;
   }
 
+  const isDarkMode = resolvedTheme === 'dark';
+
+  const toggleTheme = () => {
+    setTheme(isDarkMode ? 'light' : 'dark');
+  };
+
   return (
-    <button
-      aria-label={
-        resolvedTheme === 'dark' ? 'ライトモードに切り替える' : 'ダークモードに切り替える'
-      }
-      className={`${className} c-button c-button--icon`}
-      onClick={toggleTheme}
-      title={resolvedTheme === 'dark' ? 'ライトモードに切り替える' : 'ダークモードに切り替える'}
+    <label
+      title={isDarkMode ? 'ライトモードに切り替える' : 'ダークモードに切り替える'}
+      htmlFor='theme-toggle'
+      // 外部のクラスとモジュールのクラスを結合
+      className={`${className || ''} ${styles.themeSwitch}`}
     >
-      {resolvedTheme === 'dark' ? <HiMoon /> : <HiSun />}
-    </button>
+      <input
+        aria-label={isDarkMode ? 'ライトモードに切り替える' : 'ダークモードに切り替える'}
+        checked={isDarkMode}
+        className={styles.themeSwitchInput}
+        id='theme-toggle'
+        onChange={toggleTheme}
+        type='checkbox'
+      />
+      <span className={styles.themeSwitchSlider}>
+        <span className={styles.themeSwitchIcon}>{isDarkMode ? <HiMoon /> : <HiSun />}</span>
+      </span>
+    </label>
   );
 };
