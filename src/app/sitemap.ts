3a63a84c2159a44ab3301654cdf2e6c82a3671ff
花南地区盆踊り実行委員会 <hanaminamibonodori@gmail.com>
2025-07-11 01:29:31 +0900

    fix: sitemapエラーの修正

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,11 +3,11 @@ import { MetadataRoute } from 'next';
 import { getNewsList } from '@/libs/microCMS';
 import { NewsData } from '@/types';
 
-const BASE_URL = process.env.NEXT_PUBLIC_DOMAIN || 'https://hanabon.vercel.app';
-
 export const revalidate = 60;
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const BASE_URL = process.env.NEXT_PUBLIC_DOMAIN || 'https://hanabon.vercel.app';
+
   const defaultPages: MetadataRoute.Sitemap = [
     {
       url: BASE_URL,
